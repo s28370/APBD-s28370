8a81ef313411c47d90fc9c67065c23888a380308
s28370 <s28370@pjwstk.edu.pl>
2024-03-05 14:42:57 +0100

    My first commit

---
+++ Lab0/Lab0/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter integer: ");
+if(int.TryParse(Console.ReadLine(), out var a))
+{
+    switch (a)
+    {
+        case > 0:
+            Console.WriteLine("Int is bigger than 0");
+            break;
+        case < 0:
+            Console.WriteLine("Int is smaller than 0");
+            break;
+        default:
+            Console.WriteLine("Int equals 0");
+            break;
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter an integer");
+}
