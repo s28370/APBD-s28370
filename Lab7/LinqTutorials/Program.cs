a96a2c681f5bbd5e417c3b89bc82553e0de27599
Maksym <makcim2004leb@gmail.com>
2024-05-13 14:40:37 +0200

    Lab7 done

---
+++ Lab7/LinqTutorials/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task3();
+            var t2 = LinqTasks.Task16();
+            foreach (var o in t2)
+            {
+                Console.WriteLine(o);
+            }
+        }
+    }
+}
